18728ae910681838c03c616abbeaf695829cfdce
Rafael Nascimento <rafael.nascimento@use-move.com>
2026-08-17 22:42:48 -0300

    First commit

---
+++ src/main/java/com/verzel/challengebackend/ChallengebackendApplication.java
@@ -0,0 +1,13 @@
+package com.verzel.challengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verzel/challengebackend/ChallengebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verzel.challengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
